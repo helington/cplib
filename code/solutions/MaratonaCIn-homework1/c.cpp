d3803af476a3cf2e88d1250488197ba08e019d0b
helington <willamy.helington@gmail.com>
2025-10-28 11:30:52 -0300

    feat: add c++ template solutions

---
+++ code/solutions/MaratonaCIn-homework1/c.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Helington Willamy on 27/10/25.
+//
---
+++ code/template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+
+using namespace std;
+
+using ll = long long;
+
+int solve()
+{
+    return 0;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+}
