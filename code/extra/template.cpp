d46e771f0dac2b8ce7f605a0e887b93f009001e3
helington <willamy.helington@gmail.com>
2026-01-05 18:28:34 -0300

    feat: add code template for solutions

---
+++ code/extra/template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define MOD 1000000007
+
+void solve() {
+    // Code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // File I/O (Useful for USACO or specific contests)
+    // freopen("input.in", "r", stdin); 
+    // freopen("output.out", "w", stdout);
+
+    int tc = 1;
+    // cin >> tc; // Uncomment if the problem has multiple test cases
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
