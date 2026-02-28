68904008689a186b8c532c43d34fa4d294cffd2e
helington <willamy.helington@gmail.com>
2026-02-28 18:25:47 -0300

    feat: improve MinQueue implementation

--- code/data structures/MinQueue.cpp
+++ code/data structures/MinQueue.cpp
@@ -1,34 +1,29 @@
-// MinQueue 
+#include <bits/stdc++.h>
 
-// - Uses "Index/Timestamp" logic to track element validity.
+using namespace std;
+
+// MinQueue
+
 // - push(x): Adds element to Queue.
 // - pop(): Removes element from queue. (removes oldest element).
 // - min(): Returns minimum Queue.
 
 // O(1) amortized per operation.
 
-struct MinQueue : deque<pair<int, int>> {
-    int cnt_added;
-    int cnt_removed;
+template<typename T>
+struct MinQueue : deque<pair<T, int>> {
+    T min() { return this->front().first; }
 
-    MinQueue() : cnt_added(0), cnt_removed(0) {}
-
-    int min() {
-        return front().first;
-    }
-
-    void push(int new_element) {
-        while (!empty() && back().first > new_element)
-            q.pop_back();
-        
-        push_back({new_element, cnt_added});
-        cnt_added++;
+    void push(int x) {
+        int cnt = 1;
+        while (!this->empty() and x <= this->front().first) {
+            cnt += this->back().second;
+            this->pop_back();
+        }
+        this->push_back({x, cnt});
     }
 
     void pop() {
-        if (!empty() && front().second == cnt_removed)
-            pop_front();
-        
-        cnt_removed++;
+        if (!--this->front().second) this->pop_front();
     }
 };
